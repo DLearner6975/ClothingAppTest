1d492a4e01cbd42de292a34838d6b1c05bc1a60b
Dlearner6975 <outtacontrol6975@gmail.com>
2017-04-10 14:46:09 -0500

    Adding models to the project

---
+++ Models/OrderNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClothingAppTest.Models
+{
+    public partial class OrderNumber
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int? OrderId { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
